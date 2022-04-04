35adc5690ea9e64aaa558f06a5e0938992ffddf4
Kumuthu <102839813+prabhasha2006@users.noreply.github.com>
2022-04-04 09:34:43 +0530

    Add files via upload

--- Lusifar/Base.js
+++ Lusifar/Base.js
@@ -1,1 +1,23 @@
+/* Copyright (C) 2021 KAVIYAAH - Alexa Team  ,  Lusifar whatsapp bot owner
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+kaviyaah - kavishka sandaruwan (v 8.0.0 avalable)
+*/
 
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
+
+// K.PRABHASHA
---
+++ Lusifar/index.js
@@ -0,0 +1,14 @@
+/* Copyright (C) 2021 KAVIYAAH - Alexa Team  ,  Lusifar whatsapp bot owner
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+kaviyaah - kavishka sandaruwan (v 8.0.0 avalable)
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
