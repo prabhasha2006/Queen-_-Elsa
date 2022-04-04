b156d4447b669908581d12ba15b0361d61bae483
kavishka <91202334+kavishkaya@users.noreply.github.com>
2022-04-04 10:27:34 +0530

    Fix

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ module.exports = {
     //end 
     
     
-    BRANCH: 'master',
+    BRANCH: 'main',
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
         API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
