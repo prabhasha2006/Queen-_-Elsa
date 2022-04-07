fb248bc342b18f5496581c552f3ab8204cb0ba56
Kumuthu <102839813+prabhasha2006@users.noreply.github.com>
2022-04-07 14:46:50 +0530

    Update Base.js

--- Lusifar/Base.js
+++ Lusifar/Base.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2021 KAVIYAAH - Alexa Team  ,  Lusifar whatsapp bot owner
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-kaviyaah - kavishka sandaruwan (v 8.0.0 avalable)
+K.PRABHASHA
 */
 
 'use strict';
@@ -19,5 +19,3 @@ class Base {
 }
 
 module.exports = Base;
-
-// K.PRABHASHA
